38e3bc927d44348899bbf17f825adf6fd4f70f7d
Sergio Basurco <sergio.basurco@gmail.com>
2018-06-09 17:12:20 +0200

    Resized image to fit layout. Layout dimensions hardcoded

--- scripts/photolist.py
+++ scripts/photolist.py
@@ -3,6 +3,15 @@ import argparse
 from Tkinter import *
 from PIL import ImageTk, Image
 
+
+image_layout_width = 1000
+image_layout_height = 600
+image_layout_aspect = 1000.0/600.0
+image_width = 0
+image_height = 0
+
+
+
 #fits image inside window
 def fitImage(image, window):
     rect.x = 0
@@ -31,6 +40,19 @@ def make_input_label(master, w, *args, **kwargs):
     label.pack(side=LEFT,fill=X, expand=1)
     return label
 
+def load_image_resized(path):
+    temp_image = Image.open(path)
+    aspect = float(temp_image.width) / float(temp_image.height)
+    if(aspect > image_layout_aspect):
+        image_width = image_layout_width
+        image_height = image_width / aspect
+    else:
+        image_height = image_layout_height
+        image_width = image_height * aspect
+    temp_image = temp_image.resize((int(image_width),int(image_height)), Image.ANTIALIAS)
+
+
+
 parser = argparse.ArgumentParser(description='Pack images for web deployment')
 parser.add_argument('-d', '--dir', help='Folder with the images', required=True)
 parser.add_argument('-o', '--output', help='Output YAML file', required=True)
@@ -58,30 +80,11 @@ root.title("Gallery tool")
 root.geometry("1280x720")
 root.resizable(width=FALSE,height=FALSE)
 
-layout_width = 1000
-layout_height = 600
-ui_aspect = 1000.0/600.0
 
-image_width = 0
-image_height = 0
 
-temp_image = Image.open(path)
-aspect = float(temp_image.width) / float(temp_image.height)
-print "Aspect ratio = %s" % aspect
-
-if(aspect > ui_aspect):
-    image_width = layout_width
-    image_height = image_width / aspect
-else:
-    image_height = layout_height
-    image_width = image_height * aspect
-
-temp_image = temp_image.resize((int(image_width),int(image_height)), Image.ANTIALIAS)
-img = ImageTk.PhotoImage(temp_image)
-
-print "%sx%s" % (img.width(), img.height())
-
-# widgets
+# Widgets
+#
+img = ImageTk.PhotoImage(load_image_resized(path))
 panel = Label(root, image = img)
 #
 # Frames
@@ -109,14 +112,14 @@ album_entry = Entry(album_frame)
 #
 # Labels
 #
-
 image_label = Label(image_frame, image=img)
 title_label = Label(title_frame, text="Title")
 country_label = Label(country_frame, text="Country")
 album_label = Label(album_frame, text="Album")
 
 
-
+# Layout
+#
 main_frame.pack(fill=BOTH, expand=1)
 image_frame.pack(side=LEFT, fill=BOTH, expand=1)
 input_frame.pack(side=RIGHT)
@@ -124,36 +127,21 @@ title_frame.pack()
 country_frame.pack()
 album_frame.pack()
 bottom_frame.pack(side=BOTTOM, fill=BOTH)
-
+#
 next_button.pack(side=RIGHT)
 prev_button.pack(side=LEFT)
 save_button.pack()
 discard_button.pack()
-
+#
 title_label.pack(side=LEFT)
 country_label.pack(side=LEFT)
 album_label.pack(side=LEFT)
-
+#
 title_entry.pack(side=RIGHT)
 country_entry.pack(side=RIGHT)
 album_entry.pack(side=RIGHT)
-
+#
 image_label.pack(fill=BOTH,expand=1)
-
-#save_button.pack()
-#right_frame.pack(fill=Y)
-#title_input.pack()
-#bottom_frame.pack(fill=X)
-#country_input.pack()
-#album_input.pack()
-
-
-#bottom_frame.pack(side=BOTTOM, fill=HORIZONTAL)
-#right_frame.pack(side=RIGHT)
-#next_button.pack()
-#prev_button.pack()
-#save_button.pack()
-#discard_button.pack()
 
 
 
