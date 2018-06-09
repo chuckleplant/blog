03272e6aecb5ef77f5d47ed3c7ad672b831e1e5e
Sergio Basurco <sergio.basurco@gmail.com>
2018-06-09 17:08:26 +0200

    Resized image to fit layout. Layout dimensions hardcoded

--- scripts/photolist.py
+++ scripts/photolist.py
@@ -24,6 +24,13 @@ def discardButtonPressed():
     print 'Discard'
 
 
+def make_input_label(master, w, *args, **kwargs):
+    f = Frame(master, width=w)
+    f.pack_propagate(0) # don't shrink
+    label = Label(f, *args, **kwargs)
+    label.pack(side=LEFT,fill=X, expand=1)
+    return label
+
 parser = argparse.ArgumentParser(description='Pack images for web deployment')
 parser.add_argument('-d', '--dir', help='Folder with the images', required=True)
 parser.add_argument('-o', '--output', help='Output YAML file', required=True)
@@ -49,9 +56,29 @@ path = an_image
 root = Tk()
 root.title("Gallery tool")
 root.geometry("1280x720")
-#root.resizable(width=FALSE,height=FALSE)
+root.resizable(width=FALSE,height=FALSE)
 
-img = ImageTk.PhotoImage(Image.open(path))
+layout_width = 1000
+layout_height = 600
+ui_aspect = 1000.0/600.0
+
+image_width = 0
+image_height = 0
+
+temp_image = Image.open(path)
+aspect = float(temp_image.width) / float(temp_image.height)
+print "Aspect ratio = %s" % aspect
+
+if(aspect > ui_aspect):
+    image_width = layout_width
+    image_height = image_width / aspect
+else:
+    image_height = layout_height
+    image_width = image_height * aspect
+
+temp_image = temp_image.resize((int(image_width),int(image_height)), Image.ANTIALIAS)
+img = ImageTk.PhotoImage(temp_image)
+
 print "%sx%s" % (img.width(), img.height())
 
 # widgets
@@ -65,8 +92,6 @@ input_frame = Frame(main_frame)
 title_frame = Frame(input_frame)
 country_frame = Frame(input_frame)
 album_frame = Frame(input_frame)
-
-
 bottom_frame = Frame(root)
 #
 # buttons
@@ -78,17 +103,19 @@ discard_button = Button(input_frame, text='Discard', command=discardButtonPresse
 #
 # Text input
 #
-title_entry = Entry(input_frame)
-country_entry = Entry(input_frame)
-album_entry = Entry(input_frame)
+title_entry = Entry(title_frame)
+country_entry = Entry(country_frame)
+album_entry = Entry(album_frame)
 #
 # Labels
 #
-image_label = Label(image_frame, text="Image", bg="green", fg="white")
 
-title_label = Label(input_frame, text="Title")
-country_label = Label(input_frame, text="Title")
-album_label = Label(input_frame, text="Title")
+image_label = Label(image_frame, image=img)
+title_label = Label(title_frame, text="Title")
+country_label = Label(country_frame, text="Country")
+album_label = Label(album_frame, text="Album")
+
+
 
 main_frame.pack(fill=BOTH, expand=1)
 image_frame.pack(side=LEFT, fill=BOTH, expand=1)
@@ -106,6 +133,7 @@ discard_button.pack()
 title_label.pack(side=LEFT)
 country_label.pack(side=LEFT)
 album_label.pack(side=LEFT)
+
 title_entry.pack(side=RIGHT)
 country_entry.pack(side=RIGHT)
 album_entry.pack(side=RIGHT)
@@ -131,6 +159,6 @@ image_label.pack(fill=BOTH,expand=1)
 
 
 # init values
-title_input.insert(0, 'some text')
-country_input.insert(0, 'Spain')
+title_entry.insert(0, 'some text')
+country_entry.insert(0, 'Spain')
 root.mainloop()
