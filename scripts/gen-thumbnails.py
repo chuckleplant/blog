f188e8207b1e86f55fdf69c37e239528ebbbac06
Sergio Basurco <sergio.basurco@gmail.com>
2018-07-10 12:08:51 +0200

    photography folder will have subfolders for each album. We want to create yaml descriptors for each album too and organize them into _data. Also fixed the thumnail script to handle orientation, and using the thumbnail function too instead of manual resize

--- scripts/gen-thumbnails.py
+++ scripts/gen-thumbnails.py
@@ -1,7 +1,4 @@
-# The image resizing bit of this code is based on: https://opensource.com/life/15/2/resize-images-python
-# This script is hence released with license Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
-# Original author is: Dayo Ntwari
-# This script was written by: Sergio Basurco
+# Author: Sergio Basurco
 #
 # This script creates thumbnails for a set of image file extensions. It grabs all images recursively from
 # photo_path and places the thumbnails at photo_path/thumbnails.
@@ -9,6 +6,7 @@
 import os, sys
 import PIL
 from PIL import Image
+from PIL import ExifTags
 
 cur_path = os.path.dirname(os.path.realpath(__file__))
 photo_path = os.path.join(cur_path,"../images/photography")
@@ -26,9 +24,22 @@ for folder, subs, files in os.walk(photo_path):
                 comn_path = os.path.commonprefix([photo_path, abs_file])
                 target_path = thumbnail_path + abs_file.replace(comn_path, "")
                 img = Image.open(abs_file)
-                wpercent = target_width / float(img.size[0])
-                hsize = int((float(img.size[1]) * float(wpercent)))
-                img = img.resize((target_width, hsize), PIL.Image.ANTIALIAS)
+
+                # https://coderwall.com/p/nax6gg/fix-jpeg-s-unexpectedly-rotating-when-saved-with-pil
+                if hasattr(img, '_getexif'):
+                    orientation = 0x0112
+                    exif = img._getexif()
+                    if exif is not None:
+                        orientation = exif[orientation]
+                        rotations = {
+                        3: Image.ROTATE_180,
+                        6: Image.ROTATE_270,
+                        8: Image.ROTATE_90
+                        }
+                        if orientation in rotations:
+                            img = img.transpose(rotations[orientation])
+
+                img.thumbnail((target_width, target_width), PIL.Image.ANTIALIAS)
                 target_dir = os.path.dirname(target_path)
                 if not os.path.exists(target_dir):
                     os.makedirs(target_dir)
